5d8e68b4f6741f59ad9326434be6f4be031a4daa
caio vieira <caiodmvieiragithub@gmail.com>
2019-03-02 18:20:10 -0300

    finalizando o gitCompare colocando loading e tratando erro

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import moment from 'moment';
+import api from '../../services/api';
 
 import logo from '../../assets/logo.png';
 
@@ -6,16 +8,57 @@ import { Container, Form } from './styles';
 
 import CompareList from '../../components/CompareList';
 
-const Main = () => (
-  <Container>
-    <img src={logo} alt="Github Compare" />
+export default class Main extends Component {
+  state = {
+    loading: false,
+    repositoryError: false,
+    repositoryInput: '',
+    repositories: [],
+  };
 
-    <Form>
-      <input type="text" placeholder="usuário/repositório" />
-      <button type="submit"> OK </button>
-    </Form>
-    <CompareList />
-  </Container>
-);
+  handleAddRepository = async (e) => {
+    const { repositoryInput, repositories } = this.state;
+    e.preventDefault();
 
-export default Main;
+    this.setState({ loading: true });
+
+    try {
+      const { data: repository } = await api.get(`/repos/${repositoryInput}`);
+
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
+      this.setState({
+        repositoryInput: '',
+        repositories: [...repositories, repository],
+        repositoryError: false,
+      });
+    } catch (err) {
+      this.setState({ repositoryError: true });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    const {
+      repositoryInput, repositories, repositoryError, loading,
+    } = this.state;
+    return (
+      <Container>
+        <img src={logo} alt="Github Compare" />
+
+        <Form withError={repositoryError} onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuário/repositório"
+            value={repositoryInput}
+            onChange={(e) => {
+              this.setState({ repositoryInput: e.target.value });
+            }}
+          />
+          <button type="submit">{loading ? <i className="fa fa-spinner fa-pulse" /> : 'OK'}</button>
+        </Form>
+        <CompareList repositories={repositories} />
+      </Container>
+    );
+  }
+}
